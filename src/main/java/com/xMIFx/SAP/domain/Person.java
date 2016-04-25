2577eb67ff292ca4e56f965767ec87e3999fb94c
Vladislav <bukatinvladislav@gmail.com>
2016-04-26 00:27:00 +0300

    add h2 console in web
    fix for h2 console in web

--- src/main/java/com/xMIFx/SAP/domain/Person.java
+++ src/main/java/com/xMIFx/SAP/domain/Person.java
@@ -1,5 +1,10 @@
 package com.xMIFx.SAP.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PERSONS")
 public class Person extends User {
 
     public Person(String login, String password) {
--- src/main/java/com/xMIFx/SAP/springConfigs/SpringSecurityConfig.java
+++ src/main/java/com/xMIFx/SAP/springConfigs/SpringSecurityConfig.java
@@ -13,16 +13,16 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
+                .authorizeRequests().antMatchers("/").permitAll()
+                //.anyRequest().authenticated()
+                //.and()
+                //.formLogin().loginPage("/login").permitAll()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                .logout().permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                .authorizeRequests().antMatchers("/console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
     @Autowired
