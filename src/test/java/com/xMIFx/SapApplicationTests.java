bd266b4ef671e4dad9b81cdb4c9d3e648be5a333
Vladislav <bukatinvladislav@gmail.com>
2016-04-25 21:25:11 +0300

    Initial commit

---
+++ src/test/java/com/xMIFx/SapApplicationTests.java
@@ -0,0 +1,18 @@
+package com.xMIFx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SapApplication.class)
+@WebAppConfiguration
+public class SapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
